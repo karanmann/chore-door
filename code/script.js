b7dbb6c0f4f2b485b3996379c05baa62c8e1e9dc
Karan Mann <62376840+karanmann@users.noreply.github.com>
2020-10-28 22:34:41 +0100

    Codealong done. Site working as per Codeacademy guidelines

--- code/script.js
+++ code/script.js
@@ -1,59 +1,104 @@
-let botDoorPath = 'https://content.codecademy.com/projects/chore-door/images/robot.svg'
-let beachDoorPath = 'https://content.codecademy.com/projects/chore-door/images/beach.svg'
-let spaceDoorPath = 'https://content.codecademy.com/projects/chore-door/images/space.svg'
-let closedDoorPath = 'https://content.codecademy.com/projects/chore-door/images/closed_door.svg'
+let doorImage1 = document.getElementById("door1");
+let doorImage2 = document.getElementById("door2");
+let doorImage3 = document.getElementById("door3");
+let startButton = document.getElementById("start");
 
-const doorImage1 = document.getElementById('door1')
-const doorImage2 = document.getElementById('door2')
-const doorImage3 = document.getElementById('door3')
+let botDoorPath = "https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/robot.svg";
+let beachDoorPath = "https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/beach.svg";
+let spaceDoorPath = "https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/space.svg";
+let closedDoorPath = "https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/closed_door.svg";
+let numClosedDoors = 3;
+let openDoor1;
+let openDoor2;
+let openDoor3;
+let currentlyPlaying = true;
 
-const numClosedDoors = 3
-let openDoor1
-let openDoor2
-let openDoor3
+let isBot = (door) => {
+  if (door.src === botDoorPath) {
+    return true;
+  } else {
+    return false;
+  }
+};
 
 const isClicked = (door) => {
-  functionTwo()
-}
+  if (door.src === closedDoorPath) {
+    return false;
+  } else {
+    return true;
+  }
+};
 
-const playDoor = () => {
-  numClosedDoors --
-  if(numClosedDoors === 0) {
+const playDoor = (door) => {
+  numClosedDoors--;
+  if (numClosedDoors === 0) {
+    gameOver("win");
+  } else if (isBot(door)) {
     gameOver();
   }
-}
+};
 
 const randomChoreDoorGenerator = () => {
-  const choreDoor = Math.floor(Math.random() * numClosedDoors)
-  if(choreDoor === 0) {
-    openDoor1 = botDoorPath
-    openDoor2 = beachDoorPath
-    openDoor3 = spaceDoorPath
+  let choreDoor = Math.floor(Math.random() * numClosedDoors);
+  if (choreDoor === 0) {
+    openDoor1 = botDoorPath;
+    openDoor2 = beachDoorPath;
+    openDoor3 = spaceDoorPath;
   } else if (choreDoor === 1) {
-    openDoor2 = botDoorPath
-    openDoor1 = beachDoorPath
-    openDoor3 = spaceDoorPath
-
-  } else  { (choreDoor === 2)
-    openDoor3 = botDoorPath
-    openDoor1 = beachDoorPath
-    openDoor2 = spaceDoorPath
-  } 
-}
+    openDoor2 = botDoorPath;
+    openDoor1 = beachDoorPath;
+    openDoor3 = spaceDoorPath;
+  } else {
+    openDoor3 = botDoorPath;
+    openDoor2 = spaceDoorPath;
+    openDoor1 = beachDoorPath;
+  }
+};
 
 doorImage1.onclick = () => {
-  doorImage1.src = openDoor1
-  playDoor()
-}
+  if (currentlyPlaying && !isClicked(doorImage1)) {
+    doorImage1.src = openDoor1;
+    playDoor(doorImage1);
+  }
+};
 
 doorImage2.onclick = () => {
-  doorImage2.src = openDoor2
-  playDoor()
-}
+  if (currentlyPlaying && !isClicked(doorImage2)) {
+    doorImage2.src = openDoor2;
+    playDoor(doorImage2);
+  }
+};
 
 doorImage3.onclick = () => {
-  doorImage3.src = openDoor3
-  playDoor()
-}
+  if (currentlyPlaying && !isClicked(doorImage3)) {
+    doorImage3.src = openDoor3;
+    playDoor(doorImage3);
+  }
+};
 
-randomChoreDoorGenerator()
+let startRound = () => {
+  doorImage1.src = closedDoorPath;
+  doorImage2.src = closedDoorPath;
+  doorImage3.src = closedDoorPath;
+  numClosedDoors = 3;
+  startButton.innerHTML = "Good luck!";
+  currentlyPlaying = true;
+  randomChoreDoorGenerator();
+};
+
+startButton.onclick = () => {
+  if (currentlyPlaying === false) {
+    startRound();
+  }
+};
+
+const gameOver = (status) => {
+  if (status === "win") {
+    startButton.innerHTML = "You win! Play again?";
+  } else {
+    startButton.innerHTML = "Game over! Play again?";
+  }
+  currentlyPlaying = false;
+};
+
+startRound();
