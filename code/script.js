613aa9723a0cfce2f014e1e3e33589da7810de1b
Karan Mann <62376840+karanmann@users.noreply.github.com>
2020-10-28 20:13:20 +0100

    basic structure and imports done + onclick for door

---
+++ code/script.js
@@ -0,0 +1,19 @@
+const botDoorPath = 'https://content.codecademy.com/projects/chore-door/images/robot.svg'
+const beachDoorPath = 'https://content.codecademy.com/projects/chore-door/images/beach.svg'
+const spaceDoorPath = 'https://content.codecademy.com/projects/chore-door/images/space.svg'
+
+const doorImage1 = document.getElementById('door1')
+const doorImage2 = document.getElementById('door2')
+const doorImage3 = document.getElementById('door3')
+
+doorImage1.onclick = () => {
+  doorImage1.src = botDoorPath
+}
+
+doorImage2.onclick = () => {
+  doorImage2.src = beachDoorPath
+}
+
+doorImage3.onclick = () => {
+  doorImage3.src = spaceDoorPath
+}
